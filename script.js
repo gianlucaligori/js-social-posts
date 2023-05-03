21eadb8eae3eb6e3647a3129b2360617a1ecc241
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-05-03 15:52:51 +0200

    fix null1 error showed in console

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ const posts = [
         "media": "https://unsplash.it/600/400?image=24",
         "author": {
             "name": "Luca Formicola",
-            "image": null
+            "image": "https://unsplash.it/300/300?image=29"
         },
         "likes": 56,
         "created": "2021-04-03"
